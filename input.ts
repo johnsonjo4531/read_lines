384c02f79a76fa0313e127f9c145eaa6b1815e54
Joe Sweeney <joe.sweeney224@gmail.com>
2020-05-13 15:20:50 -0600

    Deno.EOF is no longer available, switching to null

--- input.ts
+++ input.ts
@@ -11,13 +11,13 @@ export function inputReader(
 	 * the line read and at the second index a boolean indicating whether the eof
 	 * has been reached.
 	 */
-	return async function input(output: string): Promise<string | Deno.EOF> {
+	return async function input(output: string): Promise<string | null> {
 		if (output) {
 			writer.write(new TextEncoder().encode(output));
 		}
 		const { value, done } = await lineReader.next();
 		if (done) {
-			return Deno.EOF
+			return null
 		}
 		return value;
 	};
